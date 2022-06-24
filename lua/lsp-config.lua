5a3d5c2e06d248e739a28ce02ca5fb867bca7750
Cole Walker <colewalker@Coles-MacBook-Pro.local>
2022-06-24 18:25:19 -0400

    feat: add formatting on save

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -19,23 +19,42 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   --...
+    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+    vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
+    vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
+    vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
+    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+  if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+  end
 end
 
-lspconfig.tsserver.setup {
-  on_attach=on_attach
-}
-
-lspconfig.solargraph.setup {
-  on_attach=on_attach
-}
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('lspconfig')['tsserver'].setup {
-  capabilities = capabilities
+lspconfig.tsserver.setup {
+  capabilities = capabilities, 
+  on_attach = function(client, bufnr) 
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+
+    local ts_utils = require("nvim-lsp-ts-utils")
+    ts_utils.setup({})
+    ts_utils.setup_client(client)
+
+    on_attach(client, bufnr)
+  end
 }
 
-require('lspconfig')['solargraph'].setup {
+lspconfig.solargraph.setup {
+  on_attach=on_attach,
   capabilities=capabilities
 }
 
@@ -63,4 +82,14 @@ require'nvim-treesitter.configs'.setup {
     },
   },
 }
+
+local null_ls = require('null-ls')
+null_ls.setup({
+    sources = {
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.formatting.prettier,
+    },
+    on_attach = on_attach,
+})
 
