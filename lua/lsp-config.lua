801732a5dc069c7828dbc5e85ad05d8430ce0bea
Cole Walker <colewalker@Coles-MacBook-Pro.local>
2022-06-27 10:43:28 -0400

    feat: add rubocop formatting

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -89,6 +89,8 @@ null_ls.setup({
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.code_actions.eslint,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop
     },
     on_attach = on_attach,
 })
