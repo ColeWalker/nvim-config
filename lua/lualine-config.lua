cd7e6fc5caaec5de658bdf07745757b12a180ac1
Cole Walker <cole.walker@drizly.com>
2022-07-21 07:57:40 -0400

    feat: cleanup lualine

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -6,10 +6,14 @@ sections={
     path = 1,
   }
   },
+  lualine_a={},
+  lualine_b={},
   lualine_y={},
   lualine_z={}
 },
 inactive_sections={
+  lualine_a={},
+  lualine_b={},
  lualine_c={
    {
     'filename',
