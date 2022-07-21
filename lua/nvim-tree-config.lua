ede723b45cb68698084b9e222a6e724b6eb5ec87
Cole Walker <cole.walker@drizly.com>
2022-07-21 07:57:52 -0400

    feat: hide nvim tree by default

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,6 +1,6 @@
 require("nvim-tree").setup({
-  open_on_setup = true,
-  open_on_setup_file = true,
+  open_on_setup = false,
+  open_on_setup_file = false,
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
