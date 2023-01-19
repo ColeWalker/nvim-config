c69081131c025b60fcf3959e3a5b88c4ec5e48b3
Cole Walker <cole.walker@drizly.com>
2023-01-19 10:06:56 -0500

    feat: add nvim rainbow

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -102,6 +102,10 @@ require'nvim-treesitter.configs'.setup {
     enable=true,
     additional_vim_regex_highlighting = false,
   },
+  rainbow={
+      enable=true,
+      extended_mode=true
+  },
   textobjects = {
     lsp_interop = {
       enable = true,
