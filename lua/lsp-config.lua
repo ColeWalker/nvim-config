2c807e3253e5ffb7274a2b56980a3cfa73da06d3
Cole Walker <colegrwalker@gmail.com>
2023-02-04 12:00:56 -0500

    feat: enable typescript plugin

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,43 +1,33 @@
 local lspconfig = require("lspconfig")
-local cmp = require'cmp'
-
--- require("typescript").setup({
---     disable_commands = false, -- prevent the plugin from creating Vim commands
---     debug = false, -- enable debug logging for commands
---     go_to_source_definition = {
---         fallback = true, -- fall back to standard LSP definition on failure
---     },
---     server = { -- pass options to lspconfig's setup method
---         on_attach = ...,
---     },
--- })
+local cmp = require 'cmp'
 
 
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end  -- Mappings.
-  local opts = { noremap=true, silent=true } 
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end -- Mappings.
+
+  local opts = { noremap = true, silent = true }
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   --...
-    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
-    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-    vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
-    vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
-    vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
-    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
-  if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+  vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+  vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+  vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+  vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+  vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+  vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+  vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+  vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
+  vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
+  vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
+  vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+  if client.server_capabilities.documentFormattingProvider then
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format { async = false}")
   end
 end
 
@@ -46,65 +36,86 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 
 lspconfig.cssls.setup {
-  capabilities=capabilities
+  capabilities = capabilities
 }
 
 
-lspconfig.tsserver.setup {
- init_options = {
-   preferences = {
-     importModuleSpecifierPreference = "project-relative"
-   }
+ts_options = {
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = "project-relative"
+    }
   },
-  on_attach = function(client, bufnr) 
+  on_attach = function(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
-
-    
-    -- vim.lsp.buf.execute_command({command = "_typescript.organizeImports", arguments = {vim.fn.expand("%:p")}})
-    organize_imports = function()
-      vim.lsp.buf.execute_command({command = "_typescript.organizeImports", arguments = {vim.fn.expand("%:p")}})
-    end
-    
-    vim.api.nvim_create_user_command('OrganizeImports', organize_imports, {})
-    vim.keymap.set("n", "<leader>*", organize_imports, { noremap=true })
-
-    -- local es_utils = require("nvim-lsp-ts-utils")
-    -- ts_utils.setup({})
-    -- ts_utils.setup_client(client)
-
     on_attach(client, bufnr)
-
   end,
-  capabilities = capabilities, 
+  capabilities = capabilities,
 }
+
+require("typescript").setup({
+  disable_commands = false, -- prevent the plugin from creating Vim commands
+  debug = false, -- enable debug logging for commands
+  go_to_source_definition = {
+    fallback = true, -- fall back to standard LSP definition on failure
+  },
+  server = ts_options
+})
+
 
 lspconfig.solargraph.setup {
-  on_attach=  function(client, bufnr) 
+  on_attach = function(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
 
-    on_attach(client,bufnr)
+    on_attach(client, bufnr)
   end,
-  capabilities=capabilities,
+  capabilities = capabilities,
   settings = {
     solargraph = {
-      autoformat=false,
-      formatting=false
+      autoformat = false,
+      formatting = false
     }
   }
 }
+lspconfig.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+  end,
 
-require'nvim-treesitter.configs'.setup {
+}
+
+require 'nvim-treesitter.configs'.setup {
   context_commentstring = true,
-  ensure_installed="all",
-  highlight={
-    enable=true,
+  ensure_installed = "all",
+  highlight = {
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
-  rainbow={
-      enable=true,
-      extended_mode=true
+  rainbow = {
+    enable = true,
+    extended_mode = true
   },
   textobjects = {
     lsp_interop = {
@@ -131,35 +142,36 @@ require'nvim-treesitter.configs'.setup {
         ["ab"] = "@block.outer",
       },
     },
-    rainbow={
-      enable=true,
-      extended_mode=true
+    rainbow = {
+      enable = true,
+      extended_mode = true
     }
   },
 }
 
 local null_ls = require('null-ls')
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.formatting.prettierd.with({
-          disabled_filetypes = { "json" },
-        }),
-        null_ls.builtins.diagnostics.rubocop.with({
-          command="bundle",
-          -- TODO: check if .rubocop-future exists, if not, just bundle exec rubocop 
-          args = vim.list_extend({'exec', 'rubocop', '-c', '.rubocop-future.yml'}, null_ls.builtins.diagnostics.rubocop._opts.args)
-        }),
-        null_ls.builtins.formatting.rubocop.with({
-          command="bundle",
-          args = vim.list_extend({'exec', 'rubocop'}, null_ls.builtins.formatting.rubocop._opts.args)
-        }),
-    },
-    on_attach = function(client, bufnr) 
-      -- client.name="null-ls-1"
-      on_attach(client,bufnr)
-    end,
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.prettierd.with({
+      disabled_filetypes = { "json" },
+    }),
+    null_ls.builtins.diagnostics.rubocop.with({
+      command = "bundle",
+      -- TODO: check if .rubocop-future exists, if not, just bundle exec rubocop
+      args = vim.list_extend({ 'exec', 'rubocop', '-c', '.rubocop-future.yml' },
+        null_ls.builtins.diagnostics.rubocop._opts.args)
+    }),
+    null_ls.builtins.formatting.rubocop.with({
+      command = "bundle",
+      args = vim.list_extend({ 'exec', 'rubocop' }, null_ls.builtins.formatting.rubocop._opts.args)
+    }),
+  },
+  on_attach = function(client, bufnr)
+    -- client.name="null-ls-1"
+    on_attach(client, bufnr)
+  end,
 })
 
 -- null_ls.setup({
